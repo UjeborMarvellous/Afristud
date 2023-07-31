a921ddf36a6daaf63ef7538b3d224bf7edccee25
UjeborMarvellous <ujebormarvellous@gmail.com>
2023-07-31 08:19:38 -0700

    First commit

---
+++ afristud/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="text">
+        <h1>Welcome</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
